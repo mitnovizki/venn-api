e141c2a75087c93374ae19957900bc203688d82a
aim <king@Queens-iMac.local>
2021-05-17 18:29:04 +0300

    eslint-police

--- solution/expenseReportGenerator.js
+++ solution/expenseReportGenerator.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
 const { GraphQLClient } = require('graphql-request');
 const { GRAPHQL_ENDPOINT, CATEGORIES_ENDPOINT } = require('../consts');
+
 const client = new GraphQLClient(GRAPHQL_ENDPOINT);
-const cache = []
-
-
+const cache = [];
 /**
  * Generates a report object containing the total spending of the given user in the given date range.
  * The report object's keys are all the transaction categories, and the values are the total spending
@@ -28,109 +27,81 @@ const cache = []
  *                                   BILLS: 0,
  *                                   ENTERTAINMENT: 0
  *                                }
+ *
  */
-async function generateReport(username, startDate, endDate) {
+async function classifyTransactionAxios(description) {
+  return axios.post(CATEGORIES_ENDPOINT, { transactionDescription: description })
+    .then((response) => response.data.transactionCategory);
+}
 
-  if (!username) {
-    return ("username can not be empty.")
-  }
-  let query =
-    `query($user:String!, $startDate:String, $endDate:String){
-     transactions(username: $user, startDate: $startDate, endDate: $endDate){
-            amount
-            description
-          }}`
-
-  let variables = { user: username, startDate: startDate, endDate: endDate }
-
-  return await client.request(query, variables)
-    .then(report => { return getCategories(report.transactions) })
-    .catch((err) => { throw new Error(err) })
+function groupCategoriesByTotalAmount(categoryAmountFlatTable, categoryName) {
+  return categoryAmountFlatTable.reduce((totalAmountForEachCategory, currentAmountForCategory) => {
+    const category = currentAmountForCategory[categoryName];
+    if (!totalAmountForEachCategory[category]) {
+      totalAmountForEachCategory[category] = 0;
+    }
+    totalAmountForEachCategory[category] += currentAmountForCategory.amount;
+    return totalAmountForEachCategory;
+  }, {});
 }
 
 async function getCategories(transactions) {
-  let promises = []
-  let categoryAmountFlat = []
-  let promisesQueue = []
-  let result
-  let limit = 0
+  const categoryAmountFlat = [];
+  let promises = [];
+  let result;
+  let limit = 0;
 
   try {
-    // transactions.forEach(async record => {
-    for (let i = 0; i < transactions.length; i++) {
-
-      let record = transactions[i]
-
-
-      if (cache[record.description]) {
-        total.push({ 'desc': cache[record.description] || 'NO_CATEGORY', 'amount': record.amount });
-      }
-      else {
-        limit++
-        promises.push(new Promise((resolve, rej) => {
-          resolve(classifyTransactionAxios(record.description)
-            .then(classification => {
-              total.push({ 'desc': classification || 'NO_CATEGORY', 'amount': record.amount });
-              let description = record.description
+    for (let i = 0; i < transactions.length; i += 1) {
+      const singleTransaction = transactions[i];
+      if (cache[singleTransaction.description]) {
+        categoryAmountFlat.push({ desc: cache[singleTransaction.description] || 'NO_CATEGORY', amount: singleTransaction.amount });
+      } else {
+        limit += 1;
+        promises.push(new Promise((resolve) => {
+          resolve(classifyTransactionAxios(singleTransaction.description)
+            .then((categoryName) => {
+              categoryAmountFlat.push({ desc: categoryName || 'NO_CATEGORY', amount: singleTransaction.amount });
+              const { description } = singleTransaction;
               if (!cache[description]) {
-                cache[description] = classification
+                cache[description] = categoryName;
               }
-            })
-          )
-        }))
+            }));
+        }));
       }
-
-      if (limit == 10) {
-        limit = 0
-        await Promise.all(promises)
-        promises = []
+      if (limit === 10) {
+        limit = 0;
+        Promise.all(promises);
+        promises = [];
       }
     }
-
     if (limit < 10) {
-      await Promise.all(promises)
+      await Promise.all(promises);
     }
-
-    // let i, j, chunk = 10;
-    // for (i = 0, j = promises.length; i < j; i += chunk) {
-    //   promisesQueue.push(promises.slice(i, i + chunk));
-    // }
-
-    // for (let i = 0; i < promisesQueue.length; i++) {
-    //   console.log(cache)
-    //   await Promise.all(promisesQueue[i])
-    // }
-
-    result = await groupBy(total, 'desc')
-
-    return result
-  }
-
-  catch (error) {
-    throw new Error(error)
+    result = await groupCategoriesByTotalAmount(categoryAmountFlat, 'desc');
+    return result;
+  } catch (error) {
+    throw new Error(error);
   }
 }
-// return Promise.all(promises).then(() => { return groupBy(total, 'desc') })
 
-async function classifyTransactionAxios(description) {
+async function generateReport(username, startDate, endDate) {
+  if (!username) {
+    return ('username can not be empty.');
+  }
 
-  return axios.post(CATEGORIES_ENDPOINT, { "transactionDescription": description })
-    .then((response) => { return response.data.transactionCategory })
+  const query = `query($user:String!, $startDate:String, $endDate:String){
+     transactions(username: $user, startDate: $startDate, endDate: $endDate){
+            amount
+            description
+          }}`;
+
+  const variables = { user: username, startDate, endDate };
+
+  return client.request(query, variables)
+    .then((report) => getCategories(report.transactions))
+    .catch((err) => { throw new Error(err); });
 }
-
-const groupCategoriesByTotalAmount = async (categoryAmountFlatTable, key) => {
-  return categoryAmountFlatTable.reduce((total, currentAmount) => {
-
-    let category = currentAmount[key];
-    if (!total[category]) {
-      total[category] = 0;
-    }
-    total[category] += currentAmount.amount;
-    return total;
-
-  }, {});
-};
-
 module.exports = {
   generateReport
 };
